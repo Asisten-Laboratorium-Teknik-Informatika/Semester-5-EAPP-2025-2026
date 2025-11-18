52f8fe0e6094f28744fa6c39e788ed0c32385d51
stabio99e <stabio99e@gmail.com>
2025-11-18 12:01:16 +0700

    flat: update databases

--- main.py
+++ main.py
@@ -1,5 +1,57 @@
 import eel
+import hashlib
+from db import get_connection
 
 eel.init('web')
 
-eel.start('index.html', size=(900, 600))
+# Register
+@eel.expose
+def register_user(fullname, email, phone, password):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        hashed = hashlib.sha256(password.encode()).hexdigest()
+
+        sql = "INSERT INTO users (fullname, email, phone, password) VALUES (%s, %s, %s, %s)"
+        cursor.execute(sql, (fullname, email, phone, hashed))
+
+        conn.commit()
+        return "success"
+
+    except Exception as e:
+        print(e)
+        return "error"
+
+# Login
+@eel.expose
+def login_user(email, password):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        hashed = hashlib.sha256(password.encode()).hexdigest()
+        sql = "SELECT * FROM users WHERE email = %s AND password = %s"
+        cursor.execute(sql, (email, hashed))
+
+        user = cursor.fetchone()
+
+        if user:
+            return {
+                "status": "success",
+                "fullname": user["fullname"],
+                "email": user["email"]
+            }
+        else:
+            return { "status": "invalid" }
+
+    except Exception as e:
+        print(e)
+        return { "status": "error" }
+
+
+
+eel.start(
+    'index.html',
+    cmdline_args=['--start-maximized']
+)
